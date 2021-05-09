25fa2fa5b6f397988eff3fc95e954c4c99ea54a5
Kari Delmar <delmark@oregonstate.edu>
2021-05-08 18:47:04 -0700

    Added database and connected it to location page.

--- kari/app/app.py
+++ kari/app/app.py
@@ -3,47 +3,58 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import sqlite3
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+
+# Database
+# db = SQLAlchemy(app)
+
+
+
+
 # Forms
+
+
 class reviewForm(FlaskForm):
-	name = StringField('name', validators=[DataRequired()])
-	review = TextAreaField('review', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
+    review = TextAreaField('review', validators=[DataRequired()])
+
 
 class apiForm(FlaskForm):
-	inputText = TextAreaField('input text', validators=[DataRequired()])	
+    inputText = TextAreaField('input text', validators=[DataRequired()])
 
 
 # Placeholder for info from Ben's service (Wikipedia info box scraper)
 dummyInfoboxData = {
-	'Population' : '898,553',
-	'Area' : '225.08 sq mi',
-	'Elevation' : '902 ft',
-	'Demonym(s)' : 'Columbusite',
-	'Time Zone' : 'UTC-5 (EST)'
+    'Population': '898,553',
+    'Area': '225.08 sq mi',
+    'Elevation': '902 ft',
+    'Demonym(s)': 'Columbusite',
+    'Time Zone': 'UTC-5 (EST)'
 }
 
 # Placeholder for info from Judy's service (Wikipedia text scraper)
 wikiText = {
-	'text' : """The city of Columbus was named after 15th-century Italian explorer 
-				Christopher Columbus at the city's founding in 1812.[12] It is the largest 
-				city in the world named for the explorer, who sailed to and settled parts 
-				of the Americas on behalf of Isabella I of Castille and Spain. Although no 
-				reliable history exists as to why Columbus, who had no connection to the 
-				city or state of Ohio before the city's founding, was chosen as the name 
-				for the city, the book Columbus: The Story of a City indicates a state 
-				lawmaker and local resident admired the explorer enough to persuade other 
+        'text': """The city of Columbus was named after 15th-century Italian explorer
+				Christopher Columbus at the city's founding in 1812.[12] It is the largest
+				city in the world named for the explorer, who sailed to and settled parts
+				of the Americas on behalf of Isabella I of Castille and Spain. Although no
+				reliable history exists as to why Columbus, who had no connection to the
+				city or state of Ohio before the city's founding, was chosen as the name
+				for the city, the book Columbus: The Story of a City indicates a state
+				lawmaker and local resident admired the explorer enough to persuade other
 				lawmakers to name the settlement Columbus."""
 }
 
 # Navigation Bar - Label : Route
 nav = {
-	'Home' : '/',
-	'Top' : '/top',
-	'Browse' : '/browse',
-	'Submit a Review' : '/review'
+    'Home': '/',
+    'Top': '/top',
+    'Browse': '/browse',
+    'Submit a Review': '/review'
 }
 
 serviceUrl = '/service'
@@ -52,59 +63,86 @@ serviceUrl = '/service'
 # Home Page
 @app.route('/')
 def home():
-	return render_template('base.html', nav=nav, serviceUrl=serviceUrl)
+    return render_template('base.html', nav=nav, serviceUrl=serviceUrl)
 
 
 # Submit a Review Page
 @app.route('/review')
 def review():
-	form = reviewForm()
-	return render_template('review.html', nav=nav, serviceUrl=serviceUrl, form=form)
+    form = reviewForm()
+    return render_template('review.html', nav=nav, serviceUrl=serviceUrl, form=form)
 
 
 # Top 5 Page
 @app.route('/top')
 def top():
-	return render_template('top.html', nav=nav, serviceUrl=serviceUrl)
+    return render_template('top.html', nav=nav, serviceUrl=serviceUrl)
 
 
 # Search Results Page
 @app.route('/search_results')
 def search():
-	return render_template('search_results.html', nav=nav, serviceUrl=serviceUrl)
+    return render_template('search_results.html', nav=nav, serviceUrl=serviceUrl)
 
 
 # Browse Page
 @app.route('/browse')
 def browse():
-	return render_template('browse.html', nav=nav, serviceUrl=serviceUrl)
+    return render_template('browse.html', nav=nav, serviceUrl=serviceUrl)
 
 
 # Location Page
 @app.route('/location')
 def location():
-	infoboxData = dummyInfoboxData
-	locationText = wikiText.get('text')
-	location = request.args.get('location')
-	submitURL = nav.get('Submit a Review')
-	return render_template('location.html', nav=nav, serviceUrl=serviceUrl, location=location, infoboxData=infoboxData, locationText=locationText, submitURL=submitURL)
 
+    infoboxData = dummyInfoboxData
+    locationText = wikiText.get('text')
+    submitURL = nav.get('Submit a Review')
 
+    city = request.args.get('city')
+    state = request.args.get('state')
+    country = request.args.get('country')
+
+    # Connect to database and create cursor
+    conn = sqlite3.connect('app.db')
+    cur = conn.cursor()
+
+    # Get locationId based on URL query string parameters
+    locationId = cur.execute('SELECT id FROM locations WHERE city = (?)', (city,)).fetchall()
+    print('my thing is: %s' % (locationId[0]), flush=True)
+
+    # Get all reviews for this location
+    reviews = cur.execute('SELECT review FROM reviews WHERE locationId = (?)', locationId[0]).fetchall()
+    print('my reviews are: %s' % (tuple(locationId),), flush=True)
+
+    # TODO: Create function to display location name properly
+
+    return render_template(
+        'location.html',
+        nav=nav,
+        serviceUrl=serviceUrl,
+        city=city,
+        infoboxData=infoboxData,
+        locationText=locationText,
+        submitURL=submitURL,
+        reviews=reviews)
+
+()
 # Sentiment/Text Analysis GUI
 @app.route('/service', methods=['GET', 'POST'])
 def service():
-	displayResults = False
-	txtInput = ''
-	form = apiForm()
-	if request.method == 'POST':
-		displayResults = True
-		txtInput = form.inputText.data
-		scores = SentimentIntensityAnalyzer().polarity_scores(txtInput)
-	return render_template('service.html', nav=nav, serviceUrl=serviceUrl, form=form, displayResults=displayResults, txtInput=txtInput)
+    displayResults = False
+    txtInput = ''
+    form = apiForm()
+    if request.method == 'POST':
+        displayResults = True
+        txtInput = form.inputText.data
+        scores = SentimentIntensityAnalyzer().polarity_scores(txtInput)
+    return render_template('service.html', nav=nav, serviceUrl=serviceUrl, form=form, displayResults=displayResults, txtInput=txtInput)
 
 
 # Note to self - This is what polarity_scores returns:
-#sentiment_dict = \
+# sentiment_dict = \
 #            {"neg": round(neg, 3),
 #             "neu": round(neu, 3),
 #             "pos": round(pos, 3),
--- kari/app/config.py
+++ kari/app/config.py
@@ -7,4 +7,11 @@ load_dotenv(path.join(basedir, '.env'))
 TESTING = True
 DEBUG = True
 FLASK_ENV = 'development'
+
+# Flask-WTF requires this to be set.
 SECRET_KEY = environ.get('SECRET_KEY')
+
+# SQLAlchemy
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'sqlite:///' + path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
